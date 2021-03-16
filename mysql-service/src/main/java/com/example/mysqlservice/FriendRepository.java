2d5d26d28cf88c75072b26d72a314907c82a5e3a
Enrique V. Kortright <ekortright@Enriques-MBP.attlocal.net>
2021-03-16 12:10:38 -0500

    first commit

---
+++ mysql-service/src/main/java/com/example/mysqlservice/FriendRepository.java
@@ -0,0 +1,7 @@
+package com.example.mysqlservice;
+import org.springframework.data.repository.CrudRepository;
+import com.example.mysqlservice.Friend;
+ 
+public interface FriendRepository extends CrudRepository<Friend, Integer> {
+}
+
---
+++ mysql-service/src/main/java/com/example/mysqlservice/MainController.java
@@ -0,0 +1,59 @@
+package com.example.mysqlservice;
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import co.elastic.apm.api.CaptureSpan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+ 
+@Controller
+@RequestMapping(path="/friend")
+public class MainController {
+ @Autowired
+ private FriendRepository friendRepository;
+ 
+ Logger logger = LoggerFactory.getLogger(MainController.class);
+ 
+ @CaptureSpan(value="delay")
+ public void delay(int length) {
+   logger.warn("going to sleep for " + length + "ms.");
+   try {
+     Thread.sleep(length);
+   } catch(Exception e) {
+     e.printStackTrace();
+   }
+   logger.warn("finished sleep");
+ }
+ 
+ @PostMapping(path="/add")
+ public @ResponseBody String addNewFriend (@RequestParam String name) {
+   logger.info("adding new friend: " + name  + ".");
+   delay(1000);
+   Friend friend = new Friend();
+   friend.setName(name);
+   friendRepository.save(friend);
+   logger.info("added: " + name);
+   return "Saved";
+ }
+ 
+@GetMapping(path="/all")
+public @ResponseBody Iterable<Friend> getAllFriends() {
+   double r = Math.random();
+   // generate an unexpected delay (with prob = .01%)
+   if (r < 0.0001) {
+     r = Math.random();
+     int ms = (int)(r * 8000);
+     logger.warn("Unexpected " + ms + "ms delay start.");
+     delay(ms);
+     logger.warn("Unexpected " + ms + "ms delay end.");
+   }
+   return friendRepository.findAll();
+}
+
+}
+
